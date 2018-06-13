eeaf3ac57f2ac03482f3775e863ec06e9446552a
Michael Park <mparkcode@gmail.com>
2018-06-13 08:37:58 +0000

    Added profile page

--- accounts/views.py
+++ accounts/views.py
@@ -7,20 +7,59 @@ from .forms import UserLoginForm, UserRegistrationForm
 # Create your views here.
 def login(request):
     if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(username=username,password=password)
-        if user:
-            auth.login(request, user)
-            return redirect("/")
-        return HttpResponse("That user or password is wrong Dumbass")
-    login_form = UserLoginForm()
-    return render(request, 'accounts/login.html', {'form':login_form})
+        login_form = UserLoginForm(request.POST)
+        if login_form.is_valid():
+            u = login_form.cleaned_data['username']
+            p = login_form.cleaned_data['password']
+            user = authenticate(username=u, password=p)
+    
+            if user is not None:
+                auth.login(request, user)
+                return redirect("/")
+            else:
+                login_form.add_error(None, "Your username or password are incorrect")
+    else:
+        login_form = UserLoginForm()
+
+    return render(request, 'accounts/login.html', {'form': login_form})
+
+
     
 def register(request):
-    registration_form = UserRegistrationForm()
-    return render(request, 'accounts/register.html', {'form':registration_form})
+    if request.method=="POST":
+        registration_form = UserRegistrationForm(request.POST)
+        if registration_form.is_valid():
+            registration_form.save()
+            
+            u = registration_form.cleaned_data['username']
+            p = registration_form.cleaned_data['password1']
+            user = authenticate(username=u, password=p)
+            
+            if user is not None:
+                auth.login(request, user)
+                return redirect("/")
+            else:
+                registration_form.add_error(None, "Can't log in now, try later.")
+    else:
+        registration_form = UserRegistrationForm()
+    
+    
+    return render(request, 'accounts/register.html', {'form': registration_form})
     
 def logout(request):
     auth.logout(request)
     return redirect("/")
+    
+def profile(request):
+    return render(request, 'accounts/profile.html')
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
